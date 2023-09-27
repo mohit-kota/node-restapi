a5f3267e9840ad611fe97402889bfc3a6827af7e
[mohit kota] <indianstar725@gmail.com>
2023-09-27 22:26:54 +0530

    new updates for airbnb clone

--- index.js
+++ index.js
@@ -1,55 +1,223 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+
+const User = require("./models/User");
+const Place = require("./models/Place");
+const Booking = require("./models/Booking");
+
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const imageDownloader = require('image-downloader')
+const multer = require('multer');
+const fs = require('fs');
+
+
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "fet3054tnei9fut904#t98thie@t933ht";
+// console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 
-// Middleware for parsing JSON requests
-app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+var allowedOrigins = ['http://localhost:5173',
+                      'https://airbnb-clone-1-green.vercel.app/'];
 
-// Route for POST method
-app.post('/bfhl', (req, res) => {
-  const inputData = req.body.data;
+app.use(
+  cors({
+    credentials: true,
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
-  if (!Array.isArray(inputData)) {
-    return res.status(400).json({ error: 'Invalid input data format. Expected an array.' });
+function getUserDataFromReq(req) {
+  return new Promise((resolve,reject)=>{
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if(err) throw err;
+      resolve(userData);
+    })
+  })
+}
+
+app.use('/uploads',express.static(__dirname+'/uploads'));
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
   }
-
-  const userId = 'john_doe_17091999'; // Replace with your logic
-  const email = 'john@xyz.com'; // Replace with your logic
-  const rollNumber = 'ABCD123'; // Replace with your logic
-
-  // Filter numbers and alphabets
-  const numbers = inputData.filter(item => !isNaN(item));
-  const alphabets = inputData.filter(item => isNaN(item));
-
-  // Find the highest alphabet
-  const highestAlphabet = alphabets.length > 0 ? [alphabets.sort()[alphabets.length - 1]] : [];
-
-  const response = {
-    is_success: true,
-    user_id: userId,
-    email: email,
-    roll_number: rollNumber,
-    numbers: numbers,
-    alphabets: alphabets,
-    highest_alphabet: highestAlphabet,
-  };
-
-  res.json(response);
 });
 
-// Route for GET method
-app.get('/bfhl', (req, res) => {
-  // Hardcoded response for the GET endpoint
-  const response = {
-    operation_code: 1,
-  };
-
-  res.status(200).json(response);
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email: email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
+    } else {
+      res.status(422).json("pass not ok");
+    }
+  } else {
+    res.json("not found");
+  }
 });
 
-// Start the Express server
+app.get("/profile", (req, res) => {
+    const {token} = req.cookies;
+    if (token) {
+      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+        const {name,email,_id} = await User.findById(userData.id);
+        res.json({name,email,_id});
+      });
+    } else {
+      res.json(null);
+    }
+});
+
+app.post('/logout',(req,res)=>{
+    res.cookie('token','').json(true);
+})
+
+app.post('/upload-by-link',async (req,res) => {
+  const {link} = req.body;
+  const newName ='photo' + Date.now() + '.jpg'
+  await imageDownloader.image({
+    url:link,
+    dest:__dirname + '/uploads/'+newName,
+  });
+  res.json(newName);
+})
+
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload',photosMiddleware.array('photos',100) ,(req,res)=>{
+  const uploadedFiles = [];
+  for(let i=0;i<req.files.length;i++)
+  {
+    const {path,originalname} = req.files[i];
+    const parts = originalname.split('.');
+   
+    const ext = parts[parts.length - 1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path,newPath);
+    uploadedFiles.push(newPath.replace('uploads\\',''));
+  }
+  res.json(uploadedFiles);
+})
+
+app.post('/places', (req,res)=>{
+  const {token} = req.cookies;
+  const {title,address,addedPhotos,description
+  ,perks,extraInfo,checkIn,checkOut,maxGuests,price} = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc= await Place.create({
+      owner:userData.id,
+      title,address,photos:addedPhotos,description
+      ,perks,extraInfo,checkIn,checkOut,maxGuests,price,
+    });
+    res.json(placeDoc);
+  });
+})
+
+app.get('/user-places',(req,res)=>{
+  const {token} = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const {id} = userData;
+    res.json(await Place.find({owner:id}));
+    
+  });
+})
+
+app.get('/places/:id',async (req,res)=>{
+ const {id} = req.params;
+ res.json(await Place.findById(id))
+})
+
+app.put('/places',async (req,res)=>{
+  const {token} = req.cookies;
+  const {id,title,address,addedPhotos,description
+  ,perks,extraInfo,checkIn,checkOut,maxGuests,price} = req.body;
+  
+ 
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    
+    if(userData.id=== placeDoc.owner.toString()){
+     
+      placeDoc.set({
+        title,address,photos:addedPhotos,description
+        ,perks,extraInfo,checkIn,checkOut,maxGuests,price,
+      })  
+     await  placeDoc.save();
+      res.json('ok');
+    }
+    
+  });
+
+})
+
+app.get('/places',async (req,res) =>{
+  res.json(await Place.find());
+})
+
+app.post('/booking',async (req,res)=>{
+  const userData = await getUserDataFromReq(req);
+
+  const {place,checkIn,checkOut,numberOfGuests,name,phone,price} = req.body
+  Booking.create({place,checkIn,checkOut,numberOfGuests,name,phone,price,user:userData.id,}).then((doc)=>{
+    
+    res.json(doc);
+  }).catch((err)=>{
+    throw err;
+  }) 
+})
+
+
+
+app.get('/bookings',async (req,res)=>{
+  const userData = await getUserDataFromReq(req);
+  res.json( await  Booking.find({user:userData.id}).populate('place'));
+
+})
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
